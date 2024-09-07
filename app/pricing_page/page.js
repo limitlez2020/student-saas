75622b254c58ea2ff84aa69daea1db65543e152f
fortune.akinremi <fortuneakinremi@gmail.com>
2024-09-07 10:50:08 -0400

    Implemented the Pricing Page

--- app/pricing_page/page.js
+++ app/pricing_page/page.js
@@ -17,7 +17,7 @@ const prompt = Prompt({
 
 
 
-const FeaturesPage = () => {
+const PricingPage = () => {
 
   return (
     /* Screen Container */
@@ -31,17 +31,17 @@ const FeaturesPage = () => {
       <div className="flex flex-col items-center justify-center mt-32 mx-10 md:mx-36 lg:mx-52">
         <div className={`${monstserrat.className} flex flex-col gap-4 items-center justify-center
                          font-extrabold text-6xl md:text-7xl lg:text-8xl`}>
-          <p> COMING </p>
+          <p> IT IS </p>
           
-          <div className="flex relative hover:-rotate-6 transition-transform duration-700 ease-in-out">
+          <div className="flex relative hover:rotate-6 transition-transform duration-700 ease-in-out">
             {/* Box: */}
-            <div className="bg-[#d3aefe] px-4 py-2 border-black border-2 rotate-3
+            <div className="bg-[#fede65] px-4 py-2 border-black border-2 -rotate-6
                             rounded-lg relative z-10">
-              <p> SOON </p>
+              <p> FREE! </p>
             </div>
 
             {/* Box shadow: */}
-            <div className="absolute w-full h-full bg-black rounded-md rotate-3
+            <div className="absolute w-full h-full bg-black rounded-md -rotate-6
                             top-[6px] left-[6px]"
             >
             </div>
@@ -49,11 +49,12 @@ const FeaturesPage = () => {
         </div>
 
         <p className={`${prompt.className} text-center font-normal text-sm md:text-base lg:text-lg mt-12`}>
-          This page is currently under construction 🚧
+          Since we are still in beta phase, the app is completely free. <br/>
+          Feel free to check out our demo above
         </p>
       </div>
     </div>
   );
 }
 
-export default FeaturesPage;
+export default PricingPage;
