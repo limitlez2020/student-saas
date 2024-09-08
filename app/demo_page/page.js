d00ad38a6d6e6ce317323ca36885c2a3b277efd9
fortune.akinremi <fortuneakinremi@gmail.com>
2024-09-08 15:12:11 -0400

    Implemented the navbar for the tools in the demo page

--- app/demo_page/page.js
+++ app/demo_page/page.js
@@ -22,6 +22,12 @@ const prompt = Prompt({
 
 const DemoPage = () => {
   
+  /* Track which tool is clicked: */
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = (tool) => {
+    setIsClicked(tool);
+  }
+
   return (
     /* Screen Container */
     <div className="flex flex-col min-h-screen bg-[#efe6f7]">
@@ -45,9 +51,35 @@ const DemoPage = () => {
         </div>
 
         {/********  TOOLS  *********/}
-        <div>
+        <div className="flex flex-col gap-20">
           {/* Tools NavBar */}
+          <div className={`${prompt.className} flex flex-row justify-between items-center
+                          w-full gap-2 font-normal text-sm bg-[#9854e5]/30 px-1 py-1
+                          rounded-lg z-10 shadow-inner border-black/15 border-2`}>
+            {/* Chatbot */}
+            <button className={`px-4 py-2 rounded-md ${isClicked === "Chatbot" ? "bg-white" : ""}`}
+                    onClick={() => handleClick("Chatbot")}>
+              <p> Chatbot </p>
+            </button>
+            {/* Todo */}
+            <button className={`px-4 py-2 rounded-md ${isClicked === "Todo" ? "bg-white" : ""}`}
+                    onClick={() => handleClick("Todo")}>
+              <p> Todo </p>
+            </button>
+            {/* Flashcards */}
+            <button className={`px-4 py-2 rounded-md ${isClicked === "Flashcards" ? "bg-white" : ""}`}
+                    onClick={() => handleClick("Flashcards")}>
+              <p> Flashcards </p>
+            </button>
+          </div>
         </div>
+
+        {/* Tool */}
+        {isClicked === "Chatbot" && (
+          <div className="flex flex-col items-center gap-4">
+            <ChatBot/>
+          </div>
+        )}
       </div>
       
     </div>
