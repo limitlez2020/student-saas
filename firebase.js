4abe3e5d70e5a53545f03b15ef0ecf10457de1d8
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-10 11:23:23 -0500

    Changed the rules on firebase database so I can access the number of people on the waitlist and then made .env variables for my firebaseConfig variables

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB0Dx0TCEpGUxhSf4-kcmHwW40boAR_Ths",
-  authDomain: "student-saas.firebaseapp.com",
-  projectId: "student-saas",
-  storageBucket: "student-saas.appspot.com",
-  messagingSenderId: "301886692374",
-  appId: "1:301886692374:web:f0aaeefba4521df80a6937",
-  measurementId: "G-0YYFSB1YMP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
