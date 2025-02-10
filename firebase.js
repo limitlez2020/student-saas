9fe089312925cd5325a8ff5fad2a4a1b95c78134
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-10 16:25:08 -0500

    Put the firebaseConfig code back into the firebase.js file instead of storing them as .env local variables

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB0Dx0TCEpGUxhSf4-kcmHwW40boAR_Ths",
+  authDomain: "student-saas.firebaseapp.com",
+  projectId: "student-saas",
+  storageBucket: "student-saas.firebasestorage.app",
+  messagingSenderId: "301886692374",
+  appId: "1:301886692374:web:f0aaeefba4521df80a6937",
+  measurementId: "G-0YYFSB1YMP"
 };
 
 // Initialize Firebase
