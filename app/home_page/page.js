8342a6fdb267f7faa772b69e24919a81eb2b8586
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-11 11:55:22 -0500

    Made the footer elements justify center

--- app/home_page/page.js
+++ app/home_page/page.js
@@ -173,7 +173,7 @@ const HomePage = () => {
 
         {/*********  FOOTER:  **********/}
         <div className="flex flex-wrap border-2 border-black max-w-[1200px] w-full
-                        h-auto py-5 px-5 mb-16 rounded-3xl gap-24"
+                        h-auto py-5 px-5 mb-16 rounded-3xl gap-24 justify-center"
         >
           {/* Left Section: */}
           <div className={`${monstserrat.className} relative`}>
